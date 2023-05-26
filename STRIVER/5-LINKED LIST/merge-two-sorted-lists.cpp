af5cf09b05933818284090820d8984f94aeb2bdc
Shivangi Sehgal <83656526+shivangisehgal@users.noreply.github.com>
2023-05-26 21:42:47 +0530

    Update merge-two-sorted-lists.cpp

--- STRIVER/5-LINKED LIST/merge-two-sorted-lists.cpp
+++ STRIVER/5-LINKED LIST/merge-two-sorted-lists.cpp
@@ -1,12 +1,10 @@
 //ITERATIVE
 //TC: O(N + M)
-//SC: O(N + M)
+//SC: O(1) // we are not creating any new nodes
 
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-
-        ListNode* list3 = new ListNode();
 
         if (list1 == NULL)
             return list2;
@@ -14,59 +12,41 @@ public:
         else if (list2 == NULL)
             return list1;
 
-        ListNode* temp1 = list1;
-        ListNode* temp2 = list2;
 
-        if (temp1->val <= temp2->val) 
-        {
-            list3->next = temp1;
-            list3 = list3->next;
-            temp1 = temp1->next;
-        }
-        
-        else 
-        {
-            list3->next = temp2;
-            list3 = list3->next;
-            temp2 = temp2->next;
-        }
-
+	ListNode* list3 = new ListNode();
+	    
         ListNode* temp3 = list3;
 
-        while (temp1 != NULL && temp2 != NULL) 
+        while (list1 != NULL && list2 != NULL) 
         {
-            if (temp1->val <= temp2->val) 
+            if (list1->val <= list2->val) 
             {
-                temp3->next = temp1;
+                temp3->next = list1;
                 temp3 = temp3->next;
-                temp1 = temp1->next;
+                list1 = list1->next;
             }
             
             else 
             {
-                temp3->next = temp2;
+                temp3->next = list2;
                 temp3 = temp3->next;
-                temp2 = temp2->next;
+                list2 = list2->next;
             }
         }
 
-        while (temp1 != NULL) 
-        {
-            temp3->next = temp1;
-            temp3 = temp3->next;
-            temp1 = temp1->next;
-        }
+        if (list1 != NULL) 
+            temp3->next = list1;
 
-        while (temp2 != NULL) 
-        {
-            temp3->next = temp2;
-            temp3 = temp3->next;
-            temp2 = temp2->next;
-        }
-
+        if (list2 != NULL) 
+            temp3->next = list2;
+        
+        list3 = list3->next;
+        
         return list3;
     }
 };
+
+
 
 //RECURSIVE
 //TC: O(N + M)
