37700b1e7a9f77e1acd625561fce4141c50c4f4e
Shivangi Sehgal <83656526+shivangisehgal@users.noreply.github.com>
2023-07-21 21:27:03 +0530

    Create 18-symmetric-Tree.cpp

---
+++ STRIVER/13-TREES/18-symmetric-Tree.cpp
@@ -0,0 +1,25 @@
+//Pretty self-explanatory
+//TC: O(N)
+//SC: O(H)
+
+class Solution {
+public:
+    
+    bool helper(TreeNode* r1, TreeNode* r2) {
+        if (r1 == NULL && r2 == NULL)
+            return true;
+
+        if (r1 == NULL || r2 == NULL)
+            return false;
+
+        if (r1->val != r2->val)
+            return false;
+
+        return (helper(r1->left, r2->right) && helper(r1->right, r2->left));
+    }
+    
+    bool isSymmetric(TreeNode* root) 
+    {
+        return helper(root, root);
+    }
+};
