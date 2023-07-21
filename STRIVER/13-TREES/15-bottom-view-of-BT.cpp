4ac147de53f18de97ce1f1a1cb6ef8b07a775ec0
Shivangi Sehgal <83656526+shivangisehgal@users.noreply.github.com>
2023-07-21 20:30:03 +0530

    Create 15-bottom-view-of-BT.cpp

---
+++ STRIVER/13-TREES/15-bottom-view-of-BT.cpp
@@ -0,0 +1,42 @@
+//TC: O(N + N)
+//SC: O(N)
+
+//same as top view, just don't keep the check.
+vector<int> bottomView(BinaryTreeNode<int> * root){
+
+       if(root == NULL)
+        return {};
+    
+    vector<int> ans;
+    map<int, int> mp;
+    queue<pair<BinaryTreeNode<int>*, int>> q;
+
+    int v = 0;
+    BinaryTreeNode<int>* temp = root;
+
+    q.push({root, 0});
+
+    while(!q.empty())
+    {
+        temp = q.front().first;
+        v = q.front().second;
+        q.pop();
+        
+        
+        mp[v] = temp->data;
+        
+        if(temp->left)
+            q.push({temp->left, v - 1});
+        
+        if(temp->right)
+            q.push({temp->right, v + 1});
+    }
+
+    for(auto i : mp)
+    {
+        ans.push_back(i.second);
+    }
+
+    return ans;
+    
+}
