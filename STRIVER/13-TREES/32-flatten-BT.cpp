4b2e17f6573b7e2fb9cd24becbde99c250891a6c
Shivangi Sehgal <83656526+shivangisehgal@users.noreply.github.com>
2023-07-25 21:12:37 +0530

    Update 32-flatten-BT.cpp

--- STRIVER/13-TREES/32-flatten-BT.cpp
+++ STRIVER/13-TREES/32-flatten-BT.cpp
@@ -1,1 +1,32 @@
+//Better: using stack
 
+//Optimal: Kinda like morris
+//TC: O(N)
+//SC: O(1)
+
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        while (root) 
+        {
+            if (root->left) 
+            {
+                TreeNode* rightmost = root->left;
+                
+                while (rightmost->right) 
+                {
+                    rightmost = rightmost->right;
+                }
+                
+                
+                rightmost->right = root->right;
+                
+                root->right = root->left;
+                
+                root->left = NULL;
+            }
+            
+            root = root->right;
+        }
+    }
+};
