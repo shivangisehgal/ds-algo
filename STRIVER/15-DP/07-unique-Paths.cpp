22ccb8f84e3b65cfb738c08b91acda7e605e0331
Shivangi Sehgal <83656526+shivangisehgal@users.noreply.github.com>
2023-08-05 10:23:39 +0530

    Update 07-unique-Paths.cpp

--- STRIVER/15-DP/07-unique-Paths.cpp
+++ STRIVER/15-DP/07-unique-Paths.cpp
@@ -8,6 +8,9 @@ public:
         
         if(m < 0 || n < 0)
             return 0;
+
+        if(dp[m][n] != -1)
+            return dp[m][n];
         
         return dp[m][n] = (rec(m - 1, n, dp) + rec(m, n - 1, dp));
     }
@@ -23,17 +26,6 @@ public:
 //tab
 class Solution {
 public:
-    int rec(int m, int n, vector<vector<int>>& dp)
-    {
-        if(m == 0 && n == 0)
-            return 1;
-        
-        if(m < 0 || n < 0)
-            return 0;
-        
-        return dp[m][n] = (rec(m - 1, n, dp) + rec(m, n - 1, dp));
-    }
-    
     int uniquePaths(int m, int n) {
         
         vector<vector<int>> dp(m, vector<int>(n, 0)); //note, dp should be initialized to 0
