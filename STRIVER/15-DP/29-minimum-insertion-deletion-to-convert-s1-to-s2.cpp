5536636623df44f33095036b7399c8d0aa35186e
Shivangi Sehgal <83656526+shivangisehgal@users.noreply.github.com>
2023-08-08 00:56:30 +0530

    Create 29-minimum-insertion-deletion-to-convert-s1-to-s2.cpp

---
+++ STRIVER/15-DP/29-minimum-insertion-deletion-to-convert-s1-to-s2.cpp
@@ -0,0 +1,43 @@
+//Q. MIn number of insertion and/or deletions required to convert string 1 to strong 2.
+//observing, (n1 - lcsLen) + (n2 - lcsLen)
+
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        
+          
+        int n1 = word1.length();
+        int n2 = word2.length();
+        
+        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, -1));
+        
+        for(int i = 0; i <= n1; i++)
+            dp[i][0] = 0;
+        
+        for(int j = 0; j <= n2; j++)
+            dp[0][j] = 0;
+        
+        
+        for(int i = 1; i <= n1; i++)
+        {
+            for(int j = 1; j <= n2; j++)
+            {
+
+                
+                if(word1[i - 1] == word2[j - 1])
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                
+                else
+                    dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
+            }
+        }
+        
+        int lcsLen = dp[n1][n2];
+        
+        int change1 = n1 - lcsLen;
+        int change2 = n2 - lcsLen;
+        
+        return (change1 + change2);
+        
+    }
+};
