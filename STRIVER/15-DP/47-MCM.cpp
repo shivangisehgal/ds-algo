d4e56dea7f802e4ef036109438c3276ad0821d66
Shivangi Sehgal <83656526+shivangisehgal@users.noreply.github.com>
2023-08-11 13:57:19 +0530

    Update 47-MCM.cpp

--- STRIVER/15-DP/47-MCM.cpp
+++ STRIVER/15-DP/47-MCM.cpp
@@ -61,3 +61,36 @@ int matrixChainMultiplication(int* arr, int n) {
     vector<vector<int>> dp(N, vector<int>(N, -1));
     return rec(1, N - 1, dp, arr, N);
 }
+
+//tabu
+#include<bits/stdc++.h>
+
+
+int matrixChainMultiplication(int* arr, int n) {
+
+    //SIZE OF ARR IS GIVEN AS n + 1
+    int N = n + 1;
+    vector<vector<int>> dp(N, vector<int>(N, 0));
+    int mini = 1e9;
+    //dp[i][i] = 0
+
+    for(int i = N - 1; i >= 1; i--)
+    {
+        for(int j = i + 1; j <= N - 1; j++)
+        {
+            mini = 1e9;
+            
+            for(int k = i; k <= j - 1; k++)
+            {
+                int steps = arr[i - 1] * arr[k] * arr[j];
+                steps += dp[i][k] + dp[k + 1][j];
+
+                mini = min(mini, steps);
+            }
+
+            dp[i][j] = mini;
+        }
+    }
+    
+    return dp[1][N - 1];
+}
