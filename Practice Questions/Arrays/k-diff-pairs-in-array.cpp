192b67a28a0332d3cdeb7e8dafcb99f76edbe54d
Shivangi Sehgal <83656526+shivangisehgal@users.noreply.github.com>
2023-09-02 12:32:26 +0530

    Create k-diff-pairs-in-array.cpp

---
+++ Practice Questions/Arrays/k-diff-pairs-in-array.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    
+    bool binSearch(vector<int>& nums, int s, int target)
+    {
+        int e = nums.size() - 1;
+        
+        while(s <= e)
+        {
+            int mid = (e + s) / 2;
+            
+            if(nums[mid] == target)
+                return true;
+            
+            else if(nums[mid] < target)
+                s = mid + 1;
+            
+            else
+                e = mid - 1;
+        }
+        
+        return false;
+    }
+    
+    int findPairs(vector<int>& nums, int k) {
+        
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        
+        int count = 0;
+        
+        for(int i = 0; i < n; i++)
+        {
+            if(i > 0 && nums[i] == nums[i - 1])
+                continue;
+            
+            //find element such that it is = nums[i] + k
+            if(binSearch(nums, i + 1, k + nums[i]))
+                count++;
+        }
+        
+        return count;
+    }
+    
+};
