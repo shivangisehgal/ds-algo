1dac5c3b086c3245e9085b16979d01c8b12265af
Shivangi Sehgal <83656526+shivangisehgal@users.noreply.github.com>
2023-09-09 22:10:22 +0530

    Update 06-number-of-islands.cpp

--- STRIVER/14-GRAPHS/06-number-of-islands.cpp
+++ STRIVER/14-GRAPHS/06-number-of-islands.cpp
@@ -1,6 +1,7 @@
 //on gfg - 8 directions, on leetcode - 4 directions
 //count components in matrix => perform bfs/dfs traversal on the matrix (consider all 8 directions)
 
+//BFS (gfg)
 class Solution {
   public:
   
@@ -64,3 +65,49 @@ class Solution {
         
     }
 }
+
+
+//DFS (LC)
+class Solution {
+    
+    void dfs(int i, int j, vector<vector<bool>>& visited, vector<vector<char>>& grid, int n, int m)
+    {
+        if(i < 0 || j < 0 || i >= n || j >= m || visited[i][j] == true || grid[i][j] == '0')
+            return;
+        
+        
+        visited[i][j] = true;
+        
+         dfs(i, j + 1, visited, grid, n, m);
+         dfs(i, j - 1, visited, grid, n, m);
+         dfs(i - 1, j, visited, grid, n, m);
+         dfs(i + 1, j, visited, grid, n, m);
+        
+    }
+    
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        
+        int n = grid.size();
+        int m = grid[0].size();
+
+        vector<vector<bool>> visited(n, vector<bool>(m, false));
+
+        int count = 0;
+
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < m; j++)
+            {
+                if(visited[i][j] == false && grid[i][j] == '1')
+                {
+                    count++;
+
+                    dfs(i, j, visited, grid, n, m);
+                }
+            }
+        }
+
+        return count;
+    }
+};
