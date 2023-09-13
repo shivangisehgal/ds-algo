149162402be587f4bb915622b99d5974423ee0ef
Shivangi Sehgal <83656526+shivangisehgal@users.noreply.github.com>
2023-09-13 11:15:23 +0530

    Update 15-is-Graph-Bipartite.cpp

--- STRIVER/14-GRAPHS/15-is-Graph-Bipartite.cpp
+++ STRIVER/14-GRAPHS/15-is-Graph-Bipartite.cpp
@@ -1,6 +1,6 @@
 //A bipartite graph is one, when colored using two colors, it can be done so such that no two adjacent nodes have the same color.
 //TC: O(N + 2E)
-
+//BFS
 class Solution {
     
     bool bfs(int start, vector<int>& color, vector<vector<int>>& graph)
@@ -46,6 +46,51 @@ public:
             if(color[i] == -1)
             {
                 if(bfs(i, color, graph) == false)
+                    return false;
+            }
+        }
+        
+        return true;
+    }
+};
+
+//DFS
+class Solution {
+    
+    bool dfs(int node, int nodeCol, vector<int>& color, vector<vector<int>>& graph)
+    {
+        color[node] = nodeCol;
+        
+        for(int neighbour : graph[node])
+        {
+            if(color[neighbour] == -1)
+            {
+                if(dfs(neighbour, !nodeCol, color, graph) == false)
+                    return false;
+            }
+                
+            else if(color[neighbour] == color[node])
+                return false;
+        }
+        
+        return true;
+    }
+    
+public:
+    
+    bool isBipartite(vector<vector<int>>& graph) {
+        
+        //graph is the adjacency list
+        
+        int n = graph.size();
+        
+        vector<int> color(n, -1);
+        
+        for(int i = 0; i < n; i++)
+        {
+            if(color[i] == -1)
+            {
+                if(dfs(i, 0, color, graph) == false)
                     return false;
             }
         }
