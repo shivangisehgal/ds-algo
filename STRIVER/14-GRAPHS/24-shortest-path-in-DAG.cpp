147ca3cc925aa51b77797e52291991c3abc13a96
Shivangi Sehgal <83656526+shivangisehgal@users.noreply.github.com>
2023-09-22 20:37:05 +0530

    Create 24-shortest-path-in-DAG.cpp

---
+++ STRIVER/14-GRAPHS/24-shortest-path-in-DAG.cpp
@@ -0,0 +1,76 @@
+//Refer handwritten notes
+
+#include<stack>
+
+void dfs(int node, stack<int>& topoSort, vector<int>& visited, vector<vector<pair<int, int>>>& adjList)
+{
+    if(visited[node] == 0)
+    {
+        visited[node] = 1;
+
+        for(auto neighbour_wt : adjList[node])
+        {
+            dfs(neighbour_wt.first, topoSort, visited, adjList);
+        }
+
+        topoSort.push(node);
+    }
+}
+
+vector<int> shortestPathInDAG(int n, int m, vector<vector<int>> &edges)
+{
+    //create a DAG
+    vector<vector<pair<int, int>>> adjList(n);
+
+    for(int i = 0; i < m; i++)
+    {
+        int u = edges[i][0];
+        int v = edges[i][1];
+        int wt = edges[i][2];
+
+        adjList[u].push_back({v, wt});
+    }
+
+    stack<int> topoSort;
+    vector<int> visited(n, 0);
+
+    for(int i = 0; i < n; i++)
+    {
+        if(visited[i] == 0)
+            dfs(i, topoSort, visited, adjList);
+    }
+
+    //topo sort: 0 1 2
+
+    vector<int> dist(n, INT_MAX);
+
+    int source_node = 0; //NOTE: QUESTION STATES SOURCE NODE IS 0.
+    
+    dist[source_node] = 0;
+
+    while(!topoSort.empty())
+    {
+        int node = topoSort.top();
+        topoSort.pop();
+
+        for(auto neighbour_wt : adjList[node])
+        {
+            int neighbour = neighbour_wt.first;
+            int wt = neighbour_wt.second;
+
+            if(dist[node] == INT_MAX)
+                continue;
+
+            else if(dist[node] + wt < dist[neighbour])
+                dist[neighbour] = dist[node] + wt;
+        }
+    }
+
+    for(int i = 0; i < n; i++)
+    {
+        if(dist[i] == INT_MAX)
+            dist[i] = -1;
+    }
+
+    return dist;
+}
