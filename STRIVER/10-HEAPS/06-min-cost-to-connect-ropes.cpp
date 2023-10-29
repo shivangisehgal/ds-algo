da97ee1f46f81b9b682f624c89b46010204d56b4
Shivangi Sehgal <83656526+shivangisehgal@users.noreply.github.com>
2023-10-29 15:02:29 +0530

    Create 06-min-cost-to-connect-ropes.cpp

---
+++ STRIVER/10-HEAPS/06-min-cost-to-connect-ropes.cpp
@@ -0,0 +1,37 @@
+//TC: O(NlogN)
+//SC: O(N)
+
+//connect minimum two ropes, push their sum in the minHeap.
+//this sum is added to total cost
+
+long long connectRopes(int* arr, int n)
+{
+    // Your code goes here
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+
+    for(int i = 0; i < n; i++)
+        minHeap.push(arr[i]);
+
+    int cost = 0;
+    int r1 = 0, r2 = 0;
+
+    while(!minHeap.empty())
+    {
+        r1 = minHeap.top();
+        minHeap.pop();
+
+        if(minHeap.empty())
+            return cost;
+
+        else
+        {
+            r2 = minHeap.top();
+            minHeap.pop();
+        }
+
+        cost += (r1 + r2);
+        minHeap.push(r1 + r2);
+    }
+
+    return cost;
+}
