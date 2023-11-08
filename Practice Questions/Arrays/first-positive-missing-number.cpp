196411567767a26ac9b5a952be6f760e578df0b5
Shivangi Sehgal <83656526+shivangisehgal@users.noreply.github.com>
2023-11-08 23:55:55 +0530

    Update first-positive-missing-number.cpp

--- Practice Questions/Arrays/first-positive-missing-number.cpp
+++ Practice Questions/Arrays/first-positive-missing-number.cpp
@@ -1,3 +1,41 @@
+//BRUTE
+//Sort, check for mismatch
+
+    int firstMissingPositive(vector<int>& arr) {
+        sort( arr.begin(), arr.end() );
+        int n = arr.size();
+        int smallest = 1;
+        for (int i = 0; i<n; i++) {
+            if (arr[i] > 0 && smallest == arr[i]) {
+                smallest++;
+            }
+        }
+        return smallest;
+    }
+
+//BETTER
+//Store in map
+//TC: O(N)
+//SC: O(N)
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int, int> mp;
+        
+        for (int i = 0; i < n; i++) {
+            if ( nums[i] > 0)
+                mp[nums[i]]++;
+        }
+        
+        for (int i = 1; i<=n; i++) {
+            if ( mp.count(i) == 0)
+                return i;
+        }
+        return n+1;
+    }
+};
+//APPROACH 1 - NEGATIVE MARKING
 //first poitive umber will lie from 1 to n + 1
 //also, we don't care about negatives or numbers > n + 1 (technically, n, so in case we find all 1 to n, our answer would obviously be n + 1)
 //array of size n, ans we care about [1, n] => negative marking
@@ -6,6 +44,8 @@
 // now, if index >= 0 and ind < n => a. index is valid, b. nums[i] >= 1 & nums[i] <= n
 // if 0, mark it with an out of bound negative number, 
 
+//TC: O(N)
+//SC: O(1)
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
@@ -37,5 +77,42 @@ public:
         }
         
         return (n + 1);
+    }
+};
+
+
+//OPTIMAL 2
+//Cyclic Sort
+//Used when array elements are in range [1, n]. Place numbers on their indices. dont do anything to negative/out of bound.
+//The first index of mismatch will be ans.
+//TC: O(N)
+//SC: O(1)
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        
+        int n = nums.size();
+        int i = 0;
+        
+        int ans = n + 1;
+        
+        while(i < n)
+        {
+            if(nums[i] > 0 && nums[i] <= n && (nums[i] != nums[nums[i] - 1]))
+                swap(nums[i], nums[nums[i] - 1]);
+            else
+                i++;
+        }
+        
+        for(int i = 0; i < n; i++)
+        {
+            if(nums[i] != (i + 1))
+            {
+                ans = i + 1;
+                break;
+            }
+        }
+        
+        return ans;
     }
 };
