83aa6d808203f6cb50304d850ddc5f6d3eab36ea
Shivangi Sehgal <83656526+shivangisehgal@users.noreply.github.com>
2023-11-17 17:44:41 +0530

    Update 11-min-path-sum-2.cpp

--- STRIVER/15-DP/11-min-path-sum-2.cpp
+++ STRIVER/15-DP/11-min-path-sum-2.cpp
@@ -43,6 +43,49 @@ public:
     }
 };
 
+//tabu
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        
+        int m = matrix.size();
+        int n = matrix[0].size();
+        
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        
+        int mini = INT_MAX;
+        
+         for(int j = 0; j < n; j++)
+             dp[0][j] = matrix[0][j];
+        
+        for(int i = 1; i < m; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                int up, left = INT_MAX, right = INT_MAX;
+                
+                up = matrix[i][j] + dp[i - 1][j];
+                
+                if(j > 0)
+                    left = matrix[i][j] + dp[i - 1][j - 1];
+                
+                if(j < n - 1)
+                    right = matrix[i][j] + dp[i - 1][j + 1];
+                
+                dp[i][j] = min({up, left, right});
+            }
+        }
+        
+        
+        for(int col = 0; col < n; col++)
+        {
+            mini = min(mini, dp[m - 1][col]);
+        }
+        
+        return mini;
+    }
+};
+
 //space opt
 class Solution {
 public:
