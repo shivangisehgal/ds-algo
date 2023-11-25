118e408446b0206e795ba97394243c674edc6c6f
Shivangi Sehgal <83656526+shivangisehgal@users.noreply.github.com>
2023-11-25 21:36:14 +0530

    Update 31-morris-traversal.cpp

--- STRIVER/13-TREES/31-morris-traversal.cpp
+++ STRIVER/13-TREES/31-morris-traversal.cpp
@@ -1,1 +1,42 @@
-
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        
+        
+        vector<int> v;
+        TreeNode* curr = root;
+        
+        while(curr != NULL)
+        {
+            if(curr->left == NULL)
+            {
+                v.push_back(curr->val);
+                curr = curr->right;
+            }
+            
+            else //left child exists
+            {
+                TreeNode* temp = curr->left;
+                
+                while(temp->right != NULL && temp->right != curr)
+                    temp = temp->right;
+                
+                //r
+                if(temp->right == NULL)
+                {
+                    temp->right = curr;
+                    curr = curr->left;
+                }
+                
+                else
+                {
+                    temp->right = NULL;
+                    v.push_back(curr->val);
+                    curr = curr->right;
+                }
+            }
+        }
+        
+        return v;
+    }
+};
