ead1464e8f2252837a75ce1198cc75f6b249f1b2
Shivangi Sehgal <83656526+shivangisehgal@users.noreply.github.com>
2023-12-16 01:39:50 +0530

    Create ContainsDuplicate.cpp

---
+++ Neetcode 150/Day 1/ContainsDuplicate.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        
+        unordered_set<int> hashSet;
+        
+        for(int num : nums)
+        {
+            if(hashSet.find(num) != hashSet.end())
+                return true;
+            
+            hashSet.insert(num);
+        }
+        
+        return false;
+    }
+};
