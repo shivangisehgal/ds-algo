8e0d05e38c865702bb635ec549d8b5b93ede8d88
Shivangi Sehgal <83656526+shivangisehgal@users.noreply.github.com>
2024-01-09 21:43:42 +0530

    Update 06 Search in Rotated Sorted Array.cpp

--- Coding Minutes Level-Up/04 Searching and Sorting/06 Search in Rotated Sorted Array.cpp
+++ Coding Minutes Level-Up/04 Searching and Sorting/06 Search in Rotated Sorted Array.cpp
@@ -1,27 +1,42 @@
-//if left sorted, search in left, or, search in right
+//How is it different that normal search in rotated array?
+//Edge case: [1,3,1,1,1] (when start element, is equal to mid, and THUS, is equal to last element)
+//target 3
+//Earlier there was equality, nums[s] <= nums[mid] - > 1 <= 1 true
+//but 3 > 1 && 3 <= 1 IS FALSE -> goes to right half
+//but 3 lies in left half!
+//hence, reduce search space from left and right, so that s and e are not equal.
+
+//TC: O(N) and Theta(logn)
+//SC: O(1)
+
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        
+    
+    bool search(vector<int>& nums, int target) {
+      
         int low = 0;
         int high = nums.size() - 1;
         
-        int mid;
-        
         while(low <= high)
         {
-            
-            mid = low + (high - low) / 2;
+            int mid = low + (high - low) / 2;
             
             if(nums[mid] == target)
-                return mid;
+                return true;
             
-            //left half sorted
-            if(nums[low] <= nums[mid])
+            //case 1. low = mid = high
+            if(nums[low] == nums[mid] && nums[mid] == nums[high]) //can lie anywhere
+            {
+                low++;
+                high--;
+                continue;
+            }
+            
+            //case 2. left sorted
+            else if(nums[mid] >= nums[low])
             {
                 if(target >= nums[low] && target < nums[mid])
                     high = mid - 1;
-                
                 else
                     low = mid + 1;
             }
@@ -30,13 +45,12 @@ public:
             {
                 if(target > nums[mid] && target <= nums[high])
                     low = mid + 1;
-                
                 else
                     high = mid - 1;
             }
-                
         }
         
-        return -1;
+        return false;
+        
     }
 };
