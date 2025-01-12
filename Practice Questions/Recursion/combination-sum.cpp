449895380d038b101bb541ac951c58004665408b
Shivangi Sehgal <83656526+shivangisehgal@users.noreply.github.com>
2025-01-12 12:30:21 +0530

    Update combination-sum.cpp

--- Practice Questions/Recursion/combination-sum.cpp
+++ Practice Questions/Recursion/combination-sum.cpp
@@ -1,10 +1,6 @@
-// Time Complexity: O(2^t * k) where t is the target, k is the average length
-
-// Reason: Assume if you were not allowed to pick a single element multiple times, every element will have a couple of options: pick or not pick which is 2^n different recursion calls, also assuming that the average length of every combination generated is k. (to put length k data structure into another data structure)
-
-// Why not (2^n) but (2^t) (where n is the size of an array)?
-
+// Time Complexity: O(k * (2^t)) where t is the target, k is the average length
 // Assume that there is 1 and the target you want to reach is 10 so 10 times you can “pick or not pick” an element.
+//hence, 2^n ki jagah 2^t. and k is for inserting v into ans.
 
 // Space Complexity: O(k*x), k is the average length and x is the no. of combinations
 
